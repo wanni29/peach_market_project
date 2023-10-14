fba9e42168b22990a0d8a674e1a14f156c0d3d57
wanni30 <hubhy@nate.com>
2023-10-15 00:40:41 +0900

    Ready to the BottomNavigationBar

---
+++ lib/chating/chating_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ChatingScreen extends StatelessWidget {
+  const ChatingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("ChatingScreen"),
+      ),
+    );
+  }
+}
